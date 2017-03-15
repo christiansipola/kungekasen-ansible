ab94ca96d1f4a6fdbbb74383f6bc0d69c0a4a883
Christian Sjöström <christian@kaustik.com>
2017-03-15 21:13:34 +0100

    fix sa-learn and mark as read

--- files/sa-learn.sh
+++ files/sa-learn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sa-learn --no-sync --ham Maildir/cur
-sa-learn --no-sync --spam Maildir/.Spam/cur
+HOME_PATH=$1
+sa-learn --no-sync --ham ${HOME_PATH}/Maildir/cur
+sa-learn --no-sync --spam ${HOME_PATH}/Maildir/.Spam/cur
 sa-learn --sync
 chown spamd:users /var/log/spamassassin/.spamassassin/bayes_toks
 
